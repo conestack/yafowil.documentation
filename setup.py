55f6f63954588cbfd32a5ce3a836eac9824872ac
Florian Friesdorf <flo@chaoflow.net>
2011-03-01 20:05:02 +0100

    1.2b7

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '1.1'
+version = '1.2b7'
 shortdesc = \
 'Documentation of YAFOWIL - Yet Another Form Widget Library'
 longdesc = ""
